418a3b940a2cae0ae95c7ce295782ed72d17a456
Muhammed Afsal <auxafsal@gmail.com>
2022-02-22 01:00:27 +0530

    profile edit screen added

--- app/src/main/java/com/practies/myapplication/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/practies/myapplication/ui/profile/ProfileFragment.kt
@@ -5,15 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.practies.myapplication.R
+import com.practies.myapplication.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
-
+private lateinit var binding: FragmentProfileBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_profile, container, false)
-    }
+      //  return inflater.inflate(R.layout.fragment_profile, container, false)
+
+   binding= FragmentProfileBinding.inflate(inflater,container,false)
+
+        binding.editButton.setOnClickListener {
+            findNavController().navigate(R.id.action_profileFragment2_to_profileCompletionFragment)
+        }
+
+    return binding.root}
 }
